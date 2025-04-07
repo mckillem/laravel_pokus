3172fa0bc8ad3a35c3ddde804d0d8041a4ce0762
Matěj <mckillem@tuta.io>
2025-04-07 10:39:35 +0200

    navigace

--- laravel_web/resources/js/pages/template.tsx
+++ laravel_web/resources/js/pages/template.tsx
@@ -1,8 +1,258 @@
+//
+// export default function Template() {
+//     return (
+//         <>
+//             <header className={"mb-6 text-md"}>
+                {/*<nav className={"flex items-center gap-4"}>*/}
+                {/*    <a className={"mr-4"}>Uncode</a>*/}
+                {/*    <div className={"justify-center"}>*/}
+                {/*        <a>Demos</a>*/}
+                {/*        <a>Features</a>*/}
+                {/*        <a>Works</a>*/}
+                {/*        <a>Blog</a>*/}
+                {/*        <a>Shop</a>*/}
+                {/*        <a>Pages</a>*/}
+                {/*        <a>Extra</a>*/}
+                {/*    </div>*/}
+                {/*    <a className={"mr-0"}>Get A Quote</a>*/}
+                {/*</nav>*/}
+
+                {/*<nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">*/}
+                {/*    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">*/}
+                {/*        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">*/}
+                {/*            <span*/}
+                {/*                className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">*/}
+                {/*                Uncode*/}
+                {/*            </span>*/}
+                {/*        </a>*/}
+                {/*        <button data-collapse-toggle="navbar-dropdown" type="button"*/}
+                {/*                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"*/}
+                {/*                aria-controls="navbar-dropdown" aria-expanded="false">*/}
+                {/*            <span className="sr-only">Open main menu</span>*/}
+                {/*            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"*/}
+                {/*                 viewBox="0 0 17 14">*/}
+                {/*                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"*/}
+                {/*                      stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>*/}
+                {/*            </svg>*/}
+                {/*        </button>*/}
+                {/*        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">*/}
+                {/*            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"*/}
+                {/*                       aria-current="page">*/}
+                {/*                        Demos*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"*/}
+                {/*                            className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">*/}
+                {/*                        Features*/}
+                {/*                    </button>*/}
+
+                {/*                    <div id="dropdownNavbar"*/}
+                {/*                         className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">*/}
+                {/*                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-400"*/}
+                {/*                            aria-labelledby="dropdownLargeButton">*/}
+                {/*                            <li>*/}
+                {/*                                <a href="#"*/}
+                {/*                                   className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>*/}
+                {/*                            </li>*/}
+                {/*                            <li>*/}
+                {/*                                <a href="#"*/}
+                {/*                                   className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>*/}
+                {/*                            </li>*/}
+                {/*                            <li>*/}
+                {/*                                <a href="#"*/}
+                {/*                                   className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>*/}
+                {/*                            </li>*/}
+                {/*                        </ul>*/}
+                {/*                        <div className="py-1">*/}
+                {/*                            <a href="#"*/}
+                {/*                               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">*/}
+                {/*                                Sign out*/}
+                {/*                            </a>*/}
+                {/*                        </div>*/}
+                {/*                    </div>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">*/}
+                {/*                        Works*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">*/}
+                {/*                        Blog*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">*/}
+                {/*                        Shop*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">*/}
+                {/*                        Pages*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*                <li>*/}
+                {/*                    <a href="#"*/}
+                {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">*/}
+                {/*                        Extra*/}
+                {/*                    </a>*/}
+                {/*                </li>*/}
+                {/*            </ul>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</nav>*/}
+
+//             </header>
+//             <main>
+//                 <section>
+//                     <h1>A digital design and</h1>
+//                     <h1>product agency</h1>
+//                 </section>
+//             </main>
+//             <footer></footer>
+//         </>
+//     );
+// }
+
+
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Demos', href: '#', current: true },
+    { name: 'Features', href: '#', current: false },
+    { name: 'Works', href: '#', current: false },
+    { name: 'Blog', href: '#', current: false },
+    { name: 'Shop', href: '#', current: false },
+    { name: 'Pages', href: '#', current: false },
+    { name: 'Extra', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function Template() {
     return (
-        <>
-            templ
-        </>
-    );
+        <Disclosure as="nav" className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* Mobile menu button*/}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Open main menu</span>
+                            {/*<Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />*/}
+                            {/*<XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />*/}
+                        </DisclosureButton>
+                    </div>
+                    <div className="flex flex-1 ml-0 justify-start sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center">
+                            <a>
+                                uncode
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        aria-current={item.current ? 'page' : undefined}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'rounded-md px-3 py-2 text-sm font-medium',
+                                        )}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button
+                            type="button"
+                            className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                        >
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">View notifications</span>
+                            {/*<BellIcon aria-hidden="true" className="size-6" />*/}
+                        </button>
+
+                        {/* Profile dropdown */}
+                        <Menu as="div" className="relative ml-3">
+                            <div>
+                                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                    <span className="absolute -inset-1.5" />
+                                    <span className="sr-only">Open user menu</span>
+                                    Get A Quote
+                                </MenuButton>
+                            </div>
+                            <MenuItems
+                                transition
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                            >
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                    >
+                                        Your Profile
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                    >
+                                        Settings
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                    >
+                                        Sign out
+                                    </a>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel className="sm:hidden">
+                <div className="space-y-1 px-2 pt-2 pb-3">
+                    {navigation.map((item) => (
+                        <DisclosureButton
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            aria-current={item.current ? 'page' : undefined}
+                            className={classNames(
+                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                'block rounded-md px-3 py-2 text-base font-medium',
+                            )}
+                        >
+                            {item.name}
+                        </DisclosureButton>
+                    ))}
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    )
 }
+
